a85665b13e26ad15a8a83f9773d2c54adb86055f
lexvogelaar <lex@goal043.nl>
2016-04-13 17:23:12 +0200

    first version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # schedule
-run func X every Y at Z
+
+`run func X every Y at Z`
+
+go get -u github.com/DapperDodo/schedule
+
+// run DoIt() every night at 03:00
+Run(EveryDay, OnHour, "03", DoIt)
+
+// run DoIt() every minute, 30 seconds into the minute
+Run(EveryMinute, OnSecond, "30", DoIt)
+
+Block()
+
+
+
+import (
+	"github.com/DapperDodo/schedule"
+)
+
+func main() {
+
+	schedule.Run(schedule.EveryMinute, schedule.OnSecond, "00", importer.Import)
+
+	// start `serving`
+	schedule.Block()
+}
---
+++ schedule.go
@@ -0,0 +1,65 @@
+package schedule
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	EveryMonth  = "2006-01"
+	EveryDay    = "2006-01-02"
+	EveryHour   = "2006-01-02T15"
+	EveryMinute = "2006-01-02T15:04"
+
+	OnDay    = "02"
+	OnHour   = "15"
+	OnMinute = "04"
+	OnSecond = "05"
+)
+
+/*
+	// run DoIt() every night at 03:00
+	Run(EveryDay, OnHour, "03", DoIt)
+
+	// run DoIt() every minute, 30 seconds into the minute
+	Run(EveryMinute, OnSecond, "30", DoIt)
+
+	// start `serving`
+	Block()
+*/
+func Run(periodformat string, momentformat string, moment string, action func()) {
+
+	last := time.Now().Format(periodformat)
+	fmt.Println("\nScheduler : `last` initialized to", last)
+
+	go func() {
+
+		for {
+
+			time.Sleep(time.Second)
+
+			t := time.Now()
+
+			// skip while in last period
+			curr := t.Format(periodformat)
+			if curr == last {
+				continue
+			}
+
+			// skip until we reach the right moment
+			if moment != t.Format(momentformat) {
+				continue
+			}
+
+			// go go go!
+			last = curr
+			action()
+		}
+	}()
+}
+
+func Block() {
+	for {
+		time.Sleep(time.Second)
+	}
+}
